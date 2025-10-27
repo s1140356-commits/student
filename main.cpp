46a9456ecc34673231539c4082f9e7ab81a4389f
s1140356-commits <s1140356@dmhs.kh.edu.tw>
2025-10-27 16:25:47 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
-    std::cout<< "hello, world!" << std::endl;
+ int main() {
+     int age = 37;
+     double height = 169.5;
+     std::string name = "謝秉軒";
+    bool is_student = false;
+    std::cout << "name: " << name << std::endl;
+    std::cout << "身高: " << height << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生: " << is_student << std::endl;
+    
+
     return 0;
 }
