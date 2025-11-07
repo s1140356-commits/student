6fd8cb61e72bafe1060b2adc8e24e8ad86546066
s1140356-commits <s1140356@dmhs.kh.edu.tw>
2025-11-07 11:44:50 +0800

    Add files via upload

---
+++ main (1)99.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    
+    std::cout<<"Iamgod" << std::endl;
+    std::cout<<"I am god" << std::endl;
+    std::cout<<"I\tam\tgod" << std::endl;
+    std::cout<<"I\nam\ngod" << std::endl;
+    std::cout<<"\"Iamgod\"" << std::endl;
+    std::cout<<"\\Iamgod\\" << std::endl;
+
+    return 0;
+}
